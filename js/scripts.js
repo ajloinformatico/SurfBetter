1bb3565221f682555fa9bd4329bcef8624751008
antoniojoselojoojeda <ajloinformatico@gmail.com>
2020-11-16 20:28:31 +0100

    created gitignore and uploaded project structure

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+
+/**
+ * Open a window by passing it an url
+ * @param {String} url url to open 
+ * @param {String} nombre name of the page
+ * @param {String} tipo type of the page to open (its used to respect proportions)
+ */
+function openSecondWindow(url, nombre, tipo){
+    debugger
+    //Si el tipo es avisos legales
+    if(tipo === "legalNoticesDesk"){
+        var specs = "width=1383,height=1393,scrollbars=NO";    //XD specs    
+    }else if(tipo === "signInDesk"){
+        var specs = "width=653,height=722,scrollbars=NO";
+    }
+    window.open(url, nombre, specs);
+
+}
+
+/**
+ * Just close a windows
+ */
+function closeWindow(){
+    window.close();
+}
+
+/**
+ * open singn up pop up and close the other
+ * @param {String} url 
+ */
+function changeInUp(url){
+    window.open(url, "Sign up", "width=653,height=831,scrollbars=NO");
+    closeWindow();
+}
+
+/**
+ * open sign in pop up and close the other
+ * @param {String} url 
+ */
+function changeUpIn(url){
+    window.open(url, "Sign in", "width=653,height=722,scrollbars=NO");
+    //closeWindow();
+}
