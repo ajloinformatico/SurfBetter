e820402388f2489b5c950e0522110c88c51ed023
antoniojoselojoojeda <ajloinformatico@gmail.com>
2021-03-26 12:16:16 +0100

    env

--- surfBetter/api/app.py
+++ surfBetter/api/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS # it must be comment into deployment
-from api.HelloApiHandler import HelloApiHandler # get and post requests
+from HelloApiHandler import HelloApiHandler # get and post requests
+from flask_sqlalchemy import SQLAlchemy
+#from database.MysqlLite import *
 
 # instance of Flask indicating with staic_url_path I spacefy a diferent path for the static file
 # with static folder indicate where is my client or "templates"
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__, static_url_path='', static_folder='../frontend/build')
+
+# config Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/surfbetter.db'
+
+# DataBase
+db = SQLAlchemy(app)
 
 # its use to disable error when we make an Api request to a diferent domain
 CORS(app) # it must be comment into deployment
@@ -19,9 +27,9 @@ def serve(path):
     with send_from_directory() allow flask to send index.html dorm static_file
     declare in app
     Returns:
-        [type]: [description]
+        [static folder]: [surfbetter index]
     """
     return send_from_directory(app.static_folder,'index.html')
 
-# load rest
+# load rest Api
 api.add_resource(HelloApiHandler, '/flask/hello')
