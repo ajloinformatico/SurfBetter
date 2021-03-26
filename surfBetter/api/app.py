6649d4de5e223c8a51fece391ffc122ac59c13de
antoniojoselojoojeda <ajloinformatico@gmail.com>
2021-03-26 12:25:03 +0100

    small

--- surfBetter/api/app.py
+++ surfBetter/api/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS # it must be comment into deployment
-from HelloApiHandler import HelloApiHandler # get and post requests
+from flask_cors import CORS  # it must be comment into deployment
+from HelloApiHandler import HelloApiHandler  # get and post requests
 from flask_sqlalchemy import SQLAlchemy
-#from database.MysqlLite import *
+
+# from database.MysqlLite import *
 
 # instance of Flask indicating with staic_url_path I spacefy a diferent path for the static file
 # with static folder indicate where is my client or "templates"
@@ -16,12 +17,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/surfbetter.db'
 db = SQLAlchemy(app)
 
 # its use to disable error when we make an Api request to a diferent domain
-CORS(app) # it must be comment into deployment
+CORS(app)  # it must be comment into deployment
 api = Api(app)
 
-# Routes default 
+
+# Routes default
 # load "view"
-@app.route("/", defaults={'path':''})
+@app.route("/", defaults={'path': ''})
 def serve(path):
     """ 
     with send_from_directory() allow flask to send index.html dorm static_file
@@ -29,7 +31,8 @@ def serve(path):
     Returns:
         [static folder]: [surfbetter index]
     """
-    return send_from_directory(app.static_folder,'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
+
 
 # load rest Api
 api.add_resource(HelloApiHandler, '/flask/hello')
