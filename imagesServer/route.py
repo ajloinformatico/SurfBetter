17c0468184c10c285254ad251d86dcc13d5b8ad9
Antonio José Lojo Ojeda <57002879+ajloinformatico@users.noreply.github.com>
2021-05-21 15:14:40 +0200

    Update route.py

--- imagesServer/route.py
+++ imagesServer/route.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, send_from_directory
 import os
 api_images = Blueprint('api_images', __name__)
 
-# Continuae by https://www.youtube.com/watch?v=pOFEjJ7NQek 4.35
+
 @api_images.route("/")
 def pruena():
     return "Hola",200
